43b390e2fee5173ec8d4a6f4acad9670c14d5052
msqtt <mosquito@email.cn>
2024-09-09 01:58:41 +0800

    add jbl v0.2.0

--- flake.nix
+++ flake.nix
@@ -1,7 +1,9 @@
 {
   description = "My personal NUR repository";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  outputs = { self, nixpkgs }:
+  inputs.jbl.url = "github:orzation/jbl";
+
+  outputs = { self, nixpkgs, ... } @inputs:
     let
       systems = [
         "x86_64-linux"
@@ -14,9 +16,15 @@
       forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
     in
     {
-      legacyPackages = forAllSystems (system: import ./default.nix {
-        pkgs = import nixpkgs { inherit system; };
-      });
+      legacyPackages = forAllSystems (system:
+        let
+          defaultResult = import ./default.nix {
+            pkgs = import nixpkgs { inherit system; };
+          };
+          jblPackage = { jbl = inputs.jbl.defaultPackage.${system}; };
+        in
+          defaultResult // jblPackage
+      );
       packages = forAllSystems (system: nixpkgs.lib.filterAttrs (_: v: nixpkgs.lib.isDerivation v) self.legacyPackages.${system});
     };
 }
